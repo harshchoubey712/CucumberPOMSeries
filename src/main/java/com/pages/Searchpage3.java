6ba9d699d16c528104414934cb31e5202becd4c6
harsh choubey <harshchoubey@harshs-MacBook-Pro.local>
2021-12-27 12:52:47 +0530

    harshchby branch changes

---
+++ src/main/java/com/pages/Searchpage3.java
@@ -0,0 +1,14 @@
+package com.pages;
+
+public class Searchpage3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("searchpage");
+		
+		System.out.println("searchpage updated");
+
+	}
+
+}
