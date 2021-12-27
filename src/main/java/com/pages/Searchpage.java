8d1e59189914f7a8e2c29c18d91bd538f5b95f84
harsh choubey <harshchoubey@harshs-MacBook-Pro.local>
2021-12-27 10:42:00 +0530

    addes searchpage class

---
+++ src/main/java/com/pages/Searchpage.java
@@ -0,0 +1,13 @@
+package com.pages;
+
+public class Searchpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("searchpage");
+		
+
+	}
+
+}
